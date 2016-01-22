275c8a5c8e4a22edcae74dec1e1b2b7514e4a844
Kohei Makita <nikuzuki29@gmail.com>
2016-01-23 03:31:54 +0900

    add ReadText.java
    1-1を解いた

---
+++ ReadText.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class ReadText{
+  public static void main(String[] args){
+
+    if(args.length != 1){
+      System.out.println("java ReadText <FileName>");
+      System.exit(0);
+    }
+
+    String filename = args[0];
+
+    try{
+      BufferedReader reader = new BufferedReader(new FileReader(filename));
+      String line;
+      while((line = reader.readLine()) != null){
+        System.out.println(line);
+      }
+      reader.close();
+    } catch (FileNotFoundException e){
+      System.out.println(filename + "is not found.");
+    } catch (IOException e){
+      System.out.println(e);
+    }
+  }
+}
